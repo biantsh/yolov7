d84e9711ed0770da1792efcf9124b25e90685f33
Biant Shkololli <biant.shkololli@solaborate.com>
2023-01-06 10:44:06 +0100

    Add PyPI description

--- setup.py
+++ setup.py
@@ -8,16 +8,21 @@ def setup():
     with open('requirements.txt') as f:
         requirements = f.read().splitlines()
 
-        setuptools.setup(
-            packages=setuptools.find_packages(),
-            install_requires=requirements,
-            python_requires='>=3.10',
-            include_package_data=True,
-            author='biantsh',
-            version=VERSION,
-            name='yolov7-wky',
-            scripts=glob.glob('*.py')
-        )
+    with open('README.dst') as f:
+        long_description = f.read()
+
+    setuptools.setup(
+        packages=setuptools.find_packages(),
+        install_requires=requirements,
+        python_requires='>=3.10',
+        include_package_data=True,
+        author='biantsh',
+        version=VERSION,
+        name='yolov7-wky',
+        scripts=glob.glob('*.py'),
+        long_description=long_description,
+        long_description_content_type="text/markdown"
+    )
 
 
 if __name__ == '__main__':
